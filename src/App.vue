2bcf3dc68dd5f0bc19f21fac2627e2db23d5435a
forhad.java <forhad.mml@gmail.com>
2020-03-08 23:54:31 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+	<ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Welcome to Your helloi'
+    }
+  }
+
+}
+</script>
---
+++ src/Ninja.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <ul>
+    	<li v-for="ninja in ninjas">
+    		{{ninja}}
+    	</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      ninjas: ['Forhad','Afsana','Afsar']
+    }
+  }
+
+}
+</script>
